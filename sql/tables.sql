5a88357f3cd25678eadb4c3dac675ae974b34c5f
Kim Frans <kimfrans34@gmail.com>
2021-11-22 17:04:47 +0200

    tables in order

--- sql/tables.sql
+++ sql/tables.sql
@@ -10,16 +10,16 @@ CREATE TABLE taxi(
     foreign key (region_id) references region(id)
 );
 
+CREATE TABLE routes(
+    id serial not null primary key,
+    route_name VARCHAR(100),
+    fare decimal (10,2)
+);
+
 CREATE TABLE trip(
     id serial not null primary key,
     taxi_reg_num int,
     route_name int,
     foreign key (taxi_reg_num) references taxi(id),
     foreign key (route_name) references routes(id)
-);
-
-CREATE TABLE routes(
-    id serial not null primary key,
-    route_name VARCHAR(100),
-    fare decimal (10,2)
 );
